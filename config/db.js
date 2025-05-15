ba797d2a099d3b55195092ef5dfa0bb6ad98496d
Cktripa-source <websdeveloper2024@gmail.com>
2025-05-15 09:30:25 -0700

    db config

--- config/db.js
+++ config/db.js
@@ -1,14 +1,19 @@
 const mysql = require('mysql2')
 require('dotenv').config()
+const HOST = process.env.DB_HOST || 'sql5.freesqldatabase.com';
+const USER = process.env.DB_USER || 'sql5779023';
+const PASSWORD = process.env.DB_PASSWORD || '3wvCJhgIic';
+const DB_NAME = process.env.DB_NAME || 'sql5779023';
+const PORT = process.env.DB_PORT || 3306;
 
-const {HOST,USER,PASSWORD,DB_NAME}=process.env
-
+// Create connection
 const db = mysql.createConnection({
-    host:HOST,
-    user:USER,
-    password:PASSWORD,
-    database:DB_NAME
-})
+    host: HOST,
+    user: USER,
+    password: PASSWORD,
+    database: DB_NAME,
+    port: PORT
+});
 
 db.connect(err=>{
     if (err) {
